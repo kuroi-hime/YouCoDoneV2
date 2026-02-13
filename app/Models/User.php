fe832eb6bd73f2bb322b36325d5c0c6a557f34ce
kuroi-hime <kuroihime095@gmail.com>
2026-02-13 23:01:50 +0100

    creation du modèle User.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Billable;
 
     /**
      * The attributes that are mass assignable.
@@ -64,5 +65,10 @@ class User extends Authenticatable
     public function restaurants()
     {
         return $this->hasMany(Restaurant::class, 'id_restaurant');
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class, 'user_id', 'id');
     }
 }
