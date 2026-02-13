3b5a9898e6ddb80d616cf0b002e0b388b7f51105
kuroi-hime <kuroihime095@gmail.com>
2026-02-13 22:59:40 +0100

    definition des routes.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\PaiementController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -24,12 +26,31 @@ Route::get('/restaurants', [RestaurantController::class, 'index'])->name('home')
 
 Route::get('/restaurants/{id}', [RestaurantController::class, 'show'])->name('restaurant.show');
 Route::get('/favorites')->name('favorites');
-Route::get('/{id}/restaurants')->name('myRestaurants');
+Route::get('/{id}/restaurants', [RestaurantController::class, 'restaurantsByUser'])->name('myRestaurants');
+Route::get('/{id}/reservations', [ReservationController::class, 'reservationByUser'])->name('myReservations');
+
+Route::get('/add-restaurant', [RestaurantController::class, 'create'])->name('restaut.add');
+Route::post('/add-restaurant', [RestaurantController::class, 'store'])->name('restaut.store');
+
+Route::get('/edit-restaurant/{id}', [RestaurantController::class, 'edit'])->name('restaurant.edit');
+Route::post('/edit-restaurant/{id}', [RestaurantController::class, 'update'])->name('restaurant.update');
+
+Route::post('/delete-restaurant/{id}', [RestaurantController::class, 'destroy'])->name('restaurant.delete');
+
+Route::post('/add-reservation', [ReservationController::class, 'store'])->name('reservation.store');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/checkout', function(){
+    return view('paiements.payment-form');
+});
+
+Route::post('/checkout', [PaiementController::class, 'checkout'])->name('checkout');
+Route::get('/payment/success', [PaiementController::class, 'success'])->name('payment.success');
+Route::get('/payment/cancel', [PaiementController::class, 'cancel'])->name('payment.cancel');
 
 require __DIR__.'/auth.php';
